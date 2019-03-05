aa18d21bb9843fcd307dfcdba4da69734ac105c5
JIYOON <JIYOON@192.168.0.56>
2019-03-05 22:42:45 +0900

    20190305 - 게시판 15 - 박지윤 commit

--- src/main/java/first/sample/dao/SampleDAO.java
+++ src/main/java/first/sample/dao/SampleDAO.java
@@ -44,4 +44,13 @@ public class SampleDAO extends AbstractDAO{
     public void deleteBoard(Map<String, Object> map) throws Exception{
         update("sample.deleteBoard", map);
     }
+    
+    public void deleteFileList(Map<String, Object> map) throws Exception{
+        update("sample.deleteFileList", map);
+    }
+     
+    public void updateFile(Map<String, Object> map) throws Exception{
+        update("sample.updateFile", map);
+    }
+
 }
