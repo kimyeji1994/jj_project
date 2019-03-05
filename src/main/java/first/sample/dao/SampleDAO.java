9cf31931cde2c4d33e0d6928032a8d57d8135e7c
JIYOON <JIYOON@192.168.0.56>
2019-03-05 21:50:37 +0900

    2019-03-05 게시판 14 박지윤 commit

--- src/main/java/first/sample/dao/SampleDAO.java
+++ src/main/java/first/sample/dao/SampleDAO.java
@@ -10,6 +10,11 @@ import first.common.dao.AbstractDAO;
 @Repository("sampleDAO")
 public class SampleDAO extends AbstractDAO{
  
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectFileList(Map<String, Object> map) throws Exception{
+	    return (List<Map<String, Object>>)selectList("sample.selectFileList", map);
+	}
+
     @SuppressWarnings("unchecked")
     public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception{
         return (List<Map<String, Object>>)selectList("sample.selectBoardList", map);
